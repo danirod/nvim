dde826f04b2f5c0e746a1970f1066fe2904ab172
Dani Rodríguez <dani@danirod.es>
2024-04-02 21:28:43 +0200

    LSP inlay hints (without upgrading to 0.10)
    I know that this plugin has been archived but it works. Once nvim 0.10
    is out, this won't be required.

--- init.lua
+++ init.lua
@@ -1,24 +1,23 @@
 -- "Theme"
-vim.cmd([[
-  match ExtraWhitespace /\s\+$/
-  highlight ExtraWhitespace ctermbg=1
-  highlight Whitespace ctermfg=236
-  highlight Pmenu ctermbg=235 ctermfg=15
-  highlight Pmenusel ctermbg=240 ctermfg=15
-  highlight VertSplit cterm=NONE ctermbg=NONE ctermfg=7
-  highlight StatusLine cterm=NONE ctermfg=yellow ctermbg=darkblue
-  highlight StatusLineNC cterm=NONE ctermfg=yellow ctermbg=darkblue
-]])
+local function reload_theme()
+  vim.cmd([[
+    match ExtraWhitespace /\s\+$/
+    highlight ExtraWhitespace ctermbg=1
+    highlight Whitespace ctermfg=236
+    highlight Pmenu ctermbg=235 ctermfg=15
+    highlight Pmenusel ctermbg=240 ctermfg=15
+    highlight VertSplit cterm=NONE ctermbg=NONE ctermfg=7
+    highlight StatusLine cterm=NONE ctermfg=yellow ctermbg=darkblue
+    highlight StatusLineNC cterm=NONE ctermfg=yellow ctermbg=darkblue
+    highlight LspInlayHint ctermfg=238 guifg=238
+  ]])
+end
 
 -- The following should survive changing the colorscheme
+reload_theme()
 vim.api.nvim_create_autocmd("ColorScheme", {
   desc = "Cosmetic changes",
-  callback = function()
-    vim.cmd([[
-      highlight VertSplit cterm=NONE ctermbg=NONE
-      highlight ExtraWhitespace ctermbg=1
-    ]])
-  end,
+  callback = reload_theme,
 })
 
 -- Most of the options are set by a global $HOME/.editorconfig.
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -140,3 +140,21 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
 })
+
+-- Inline hints without having to install nvim-dev
+require("lsp-inlayhints").setup()
+vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_inlayhints",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    require("lsp-inlayhints").on_attach(client, bufnr)
+
+    vim.cmd([[highlight LspInlayHint ctermfg=238 guifg=238]])
+  end,
+})
