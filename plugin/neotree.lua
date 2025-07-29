e8fd1f4b673671628c08b482e42bb2ce196dfee2
Dani Rodríguez <dani@danirod.es>
2025-07-29 17:49:09 +0200

    Replace nerdtree with neotree

---
+++ plugin/neotree.lua
@@ -0,0 +1,16 @@
+require("neo-tree").setup({
+  close_if_last_window = true,
+  window = {
+    width = 35,
+    position = "right",
+    auto_resize = true,
+  },
+  hijack_netrw_behavior = "open_default",
+  filtered_items = {
+    hide_dotfiles = false,
+    hide_gitignore = false,
+    visible = false,
+  },
+})
+
+vim.keymap.set("n", "<Leader>nt", ":Neotree reveal toggle<CR>", { silent = true })
