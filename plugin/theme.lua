7fbfcd3811c1c5a2d21dbe4d66a3dac0efd577ba
Dani Rodríguez <dani@danirod.es>
2025-07-29 16:50:53 +0200

    Sticking to the default neovim theme

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -1,23 +1,3 @@
--- vim.o.background = "dark"
--- require("onedarkpro").setup({
---   options = {
---     transparency = true,
---   },
--- })
--- require("onedarkpro").load()
-
--- local c = require("vscode.colors").get_colors()
--- require("vscode").setup({
---   transparent = true,
---   italic_comments = false,
---   group_overrides = {
---     StatusLine = { fg = c.vscFront, bg = c.vscUiBlue },
---     StatusLineNC = { fg = c.vscGray, bg = c.vscUiBlue },
---     LspInlayHint = { fg = c.vscGray },
---   },
--- })
--- require("vscode").load()
-
 vim.cmd("hi Normal guibg=NONE")
 vim.cmd("hi ModeMsg guibg=NONE")
 vim.cmd("hi Whitespace ctermfg=blue guifg=NvimDarkGrey4")
